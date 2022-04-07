499f3e3623e3b55cfc6d8e322a3a1a72ef562fb1
sunarke <624910123@qq.com>
2022-04-07 15:58:19 -0700

    4-7 create the-footer component

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <a-layout-footer style="text-align: center">
+    ebook for CS5500 -- footer
+  </a-layout-footer>
+
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'the-footer'
+});
+</script>
