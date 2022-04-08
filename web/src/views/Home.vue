b13fb6e06a2b49e72184562da484304e8498525d
sunarke <624910123@qq.com>
2022-04-07 21:49:44 -0700

    5-4 ebook layout adjustment, three books in one row

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,76 +1,62 @@
 <template>
-  <div class="home">
-    <a-layout>
-      <a-layout-sider width="200" style="background: #fff">
-        <a-menu
-            mode="inline"
-
-            :style="{ height: '100%', borderRight: 0 }"
-        >
-          <a-sub-menu key="sub1">
-            <template #title>
-              <span><user-outlined />subnav 1</span>
-            </template>
-            <a-menu-item key="1">option1</a-menu-item>
-            <a-menu-item key="2">option2</a-menu-item>
-            <a-menu-item key="3">option3</a-menu-item>
-            <a-menu-item key="4">option4</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <template #title>
-              <span><laptop-outlined />subnav 2</span>
-            </template>
-            <a-menu-item key="5">option5</a-menu-item>
-            <a-menu-item key="6">option6</a-menu-item>
-            <a-menu-item key="7">option7</a-menu-item>
-            <a-menu-item key="8">option8</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub3">
-            <template #title>
-              <span><notification-outlined />subnav 3</span>
-            </template>
-            <a-menu-item key="9">option9</a-menu-item>
-            <a-menu-item key="10">option10</a-menu-item>
-            <a-menu-item key="11">option11</a-menu-item>
-            <a-menu-item key="12">option12</a-menu-item>
-          </a-sub-menu>
-        </a-menu>
-      </a-layout-sider>
-      <a-layout-content
-          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+  <a-layout>
+    <a-layout-sider width="200" style="background: #fff">
+      <a-menu
+          mode="inline"
+          :style="{ height: '100%', borderRight: 0 }"
       >
-        <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-          <template #footer>
-            <div><b>ant design vue</b> footer part</div>
+        <a-sub-menu key="sub1">
+          <template #title>
+            <span><user-outlined />subnav 11111</span>
           </template>
-          <template #renderItem="{ item }">
-            <a-list-item key="item.title">
-              <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px" />
-            {{ text }}
-          </span>
-              </template>
-              <template #extra>
-                <img
-                    width="272"
-                    alt="logo"
-                    src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-                />
-              </template>
-              <a-list-item-meta :description="item.description">
-                <template #title>
-                  <a :href="item.href">{{ item.title }}</a>
-                </template>
-                <template #avatar><a-avatar :src="item.avatar"/></template>
-              </a-list-item-meta>
-              {{ item.content }}
-            </a-list-item>
+          <a-menu-item key="1">option1</a-menu-item>
+          <a-menu-item key="2">option2</a-menu-item>
+          <a-menu-item key="3">option3</a-menu-item>
+          <a-menu-item key="4">option4</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub2">
+          <template #title>
+            <span><laptop-outlined />subnav 2</span>
           </template>
-        </a-list>
-      </a-layout-content>
-    </a-layout>
-  </div>
+          <a-menu-item key="5">option5</a-menu-item>
+          <a-menu-item key="6">option6</a-menu-item>
+          <a-menu-item key="7">option7</a-menu-item>
+          <a-menu-item key="8">option8</a-menu-item>
+        </a-sub-menu>
+        <a-sub-menu key="sub3">
+          <template #title>
+            <span><notification-outlined />subnav 3</span>
+          </template>
+          <a-menu-item key="9">option9</a-menu-item>
+          <a-menu-item key="10">option10</a-menu-item>
+          <a-menu-item key="11">option11</a-menu-item>
+          <a-menu-item key="12">option12</a-menu-item>
+        </a-sub-menu>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout-content
+        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
+    >
+      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+        <template #renderItem="{ item }">
+          <a-list-item key="item.name">
+            <template #actions>
+              <span v-for="{ type, text } in actions" :key="type">
+                <component v-bind:is="type" style="margin-right: 8px" />
+                {{ text }}
+              </span>
+            </template>
+            <a-list-item-meta :description="item.description">
+              <template #title>
+                <a :href="item.href">{{ item.name }}</a>
+              </template>
+              <template #avatar><a-avatar :src="item.cover"/></template>
+            </a-list-item-meta>
+          </a-list-item>
+        </template>
+      </a-list>
+    </a-layout-content>
+  </a-layout>
 </template>
 
 <script lang="ts">
@@ -92,24 +78,24 @@ for (let i = 0; i < 23; i++) {
 
 export default defineComponent({
   name: 'Home',
-  setup(){
+  setup() {
     console.log("setup");
     const ebooks = ref();
     const ebooks1 = reactive({books: []});
 
     onMounted(() => {
       console.log("onMounted");
-      axios.get("http://localhost:8880/ebook/list?name=Vue")
-          .then((response) => {
-            const data = response.data;
-            ebooks.value = data.content
-            ebooks1.books = data.content;
-            console.log(response);
-          });
-    })
+      axios.get("http://127.0.0.1:8880/ebook/list?name=Vue").then((response) => {
+        const data = response.data;
+        ebooks.value = data.content;
+        ebooks1.books = data.content;
+        console.log(response);
+      });
+    });
+
     return {
       ebooks,
-      ebooks2 : toRef(ebooks1, "books"),
+      ebooks2: toRef(ebooks1, "books"),
       listData,
       pagination: {
         onChange: (page: any) => {
@@ -123,7 +109,6 @@ export default defineComponent({
         { type: 'MessageOutlined', text: '2' },
       ],
     }
-
   }
 });
 </script>
